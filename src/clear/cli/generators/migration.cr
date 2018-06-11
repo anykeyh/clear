082a719937d6da275892a66e5d320c15c2d1578a
Yacine Petitprez <yacine.petitprez@ingedata.net>
2018-06-11 20:46:36 +0200

    Comment generator code until I fix them

--- src/clear/cli/generators/migration.cr
+++ src/clear/cli/generators/migration.cr
@@ -1,26 +1,26 @@
 
 
-Clear::CLI::Generator.add("migration",
-  "Create a new migration") do |opts|
-  g = Generate::Generator.new
-  g.target_directory = "."
+# Clear::CLI::Generator.add("migration",
+#   "Create a new migration") do |opts|
+#   g = Generate::Generator.new
+#   g.target_directory = "."
 
-  timestamp = Time.now.epoch.to_s.rjust(10, '0')
+#   timestamp = Time.now.epoch.to_s.rjust(10, '0')
 
-  name = opts.shift?
+#   name = opts.shift?
 
-  if name
-    name_underscore = name.underscore
-    class_name = name.camelcase
+#   if name
+#     name_underscore = name.underscore
+#     class_name = name.camelcase
 
-    file_name = "#{timestamp}_#{name_underscore}.cr"
-    g["class_name"] = "#{class_name}#{timestamp}"
+#     file_name = "#{timestamp}_#{name_underscore}.cr"
+#     g["class_name"] = "#{class_name}#{timestamp}"
 
-    g.in_directory "src/db/migrations" do
-      g.file(file_name, "./templates/migration.ecr")
-    end
-  else
-    puts "Please provide a name for the migration"
-    exit 1
-  end
-end
+#     g.in_directory "src/db/migrations" do
+#       g.file(file_name, "./templates/migration.ecr")
+#     end
+#   else
+#     puts "Please provide a name for the migration"
+#     exit 1
+#   end
+# end
--- src/clear/cli/generators/new/kemal.cr
+++ src/clear/cli/generators/new/kemal.cr
@@ -1,74 +1,74 @@
-require "generate"
+# require "generate"
 
-Clear::CLI::Generator.add("new/kemal",
-  "Setup a minimal application with kemal and clear") do |args|
-  g = Generate::Generator.new
+# Clear::CLI::Generator.add("new/kemal",
+#   "Setup a minimal application with kemal and clear") do |args|
+#   g = Generate::Generator.new
 
-  g.target_directory = "."
+#   g.target_directory = "."
 
-  OptionParser.parse(args) do |opts|
-    opts.on("-d", "--directory DIRECTORY", "Set directory") do |dir|
-      g.target_directory = dir
-    end
+#   OptionParser.parse(args) do |opts|
+#     opts.on("-d", "--directory DIRECTORY", "Set directory") do |dir|
+#       g.target_directory = dir
+#     end
 
-    opts.on("-n", "--name NAME", "Set application name") do |name|
-      g["app_name"] = name
-    end
-  end
+#     opts.on("-n", "--name NAME", "Set application name") do |name|
+#       g["app_name"] = name
+#     end
+#   end
 
-  g["app_name"] = g["app_name"]? || File.basename(g.target_directory)
-  g["app_name_underscore"] = g["app_name"].underscore
-  g["app_name_camelcase"] = g["app_name"].camelcase
+#   g["app_name"] = g["app_name"]? || File.basename(g.target_directory)
+#   g["app_name_underscore"] = g["app_name"].underscore
+#   g["app_name_camelcase"] = g["app_name"].camelcase
 
-  g["git_username"] = `git config user.email`.chomp || "email@example.com"
-  g["git_email"] = `git config user.name`.chomp || "Your Name"
+#   g["git_username"] = `git config user.email`.chomp || "email@example.com"
+#   g["git_email"] = `git config user.name`.chomp || "Your Name"
 
-  g.in_directory "bin" do
-    g.file "appctl", Clear::CLI::Generator.ecr_to_s("./templates/kemal/bin/appctl.ecr", g)
-    g.file "clear_cli.cr", Clear::CLI::Generator.ecr_to_s("./templates/kemal/bin/clear_cli.cr.ecr", g)
-    g.file "server.cr", Clear::CLI::Generator.ecr_to_s("./templates/kemal/bin/server.cr.ecr", g)
-  end
+#   g.in_directory "bin" do
+#     g.file "appctl", Clear::CLI::Generator.ecr_to_s("./templates/kemal/bin/appctl.ecr", g)
+#     g.file "clear_cli.cr", Clear::CLI::Generator.ecr_to_s("./templates/kemal/bin/clear_cli.cr.ecr", g)
+#     g.file "server.cr", Clear::CLI::Generator.ecr_to_s("./templates/kemal/bin/server.cr.ecr", g)
+#   end
 
-  g.in_directory "config" do
-    g.file "database.yml", Clear::CLI::Generator.ecr_to_s("./templates/kemal/config/database.yml.ecr", g)
-  end
+#   g.in_directory "config" do
+#     g.file "database.yml", Clear::CLI::Generator.ecr_to_s("./templates/kemal/config/database.yml.ecr", g)
+#   end
 
-  g.in_directory "src" do
-    g.in_directory "controllers" do
-      g.file "application_controller.cr", Clear::CLI::Generator.ecr_to_s("./templates/kemal/src/controllers/application_controller.ecr", g)
-      g.file "welcome_controller.cr", Clear::CLI::Generator.ecr_to_s("./templates/kemal/src/controllers/welcome_controller.ecr", g)
-    end
+#   g.in_directory "src" do
+#     g.in_directory "controllers" do
+#       g.file "application_controller.cr", Clear::CLI::Generator.ecr_to_s("./templates/kemal/src/controllers/application_controller.ecr", g)
+#       g.file "welcome_controller.cr", Clear::CLI::Generator.ecr_to_s("./templates/kemal/src/controllers/welcome_controller.ecr", g)
+#     end
 
-    g.in_directory "db" do
-      g.file "init.cr", Clear::CLI::Generator.ecr_to_s("./templates/kemal/src/db/init.ecr", g)
-    end
+#     g.in_directory "db" do
+#       g.file "init.cr", Clear::CLI::Generator.ecr_to_s("./templates/kemal/src/db/init.ecr", g)
+#     end
 
-    g.in_directory "models" do
-      g.file "init.cr", Clear::CLI::Generator.ecr_to_s("./templates/kemal/src/models/application_model.ecr", g)
-    end
+#     g.in_directory "models" do
+#       g.file "init.cr", Clear::CLI::Generator.ecr_to_s("./templates/kemal/src/models/application_model.ecr", g)
+#     end
 
-    g.in_directory "views" do
-      g.in_directory "components" do
-        g.file "footer.cr", Clear::CLI::Generator.ecr_to_s("./templates/kemal/src/views/components/footer.ecr", g)
-      end
+#     g.in_directory "views" do
+#       g.in_directory "components" do
+#         g.file "footer.cr", Clear::CLI::Generator.ecr_to_s("./templates/kemal/src/views/components/footer.ecr", g)
+#       end
 
-      g.in_directory "layouts" do
-        g.file "application.cr", Clear::CLI::Generator.ecr_to_s("./templates/kemal/src/views/layouts/application.ecr", g)
-      end
+#       g.in_directory "layouts" do
+#         g.file "application.cr", Clear::CLI::Generator.ecr_to_s("./templates/kemal/src/views/layouts/application.ecr", g)
+#       end
 
-      g.in_directory "welcome" do
-        g.file "index.cr", Clear::CLI::Generator.ecr_to_s("./templates/kemal/src/views/welcome/index.ecr", g)
-      end
-    end
+#       g.in_directory "welcome" do
+#         g.file "index.cr", Clear::CLI::Generator.ecr_to_s("./templates/kemal/src/views/welcome/index.ecr", g)
+#       end
+#     end
 
-    g.file "app.cr", Clear::CLI::Generator.ecr_to_s("./templates/kemal/src/app.ecr", g)
-  end
+#     g.file "app.cr", Clear::CLI::Generator.ecr_to_s("./templates/kemal/src/app.ecr", g)
+#   end
 
-  g.file ".gitignore", Clear::CLI::Generator.ecr_to_s("./templates/kemal/_gitignore.ecr", g)
-  g.file "shard.yml", Clear::CLI::Generator.ecr_to_s("./templates/kemal/shard.yml.ecr", g)
+#   g.file ".gitignore", Clear::CLI::Generator.ecr_to_s("./templates/kemal/_gitignore.ecr", g)
+#   g.file "shard.yml", Clear::CLI::Generator.ecr_to_s("./templates/kemal/shard.yml.ecr", g)
 
-  system("chmod +x #{g.target_directory}/bin/appctl")
-  system("cd #{g.target_directory} && shards")
+#   system("chmod +x #{g.target_directory}/bin/appctl")
+#   system("cd #{g.target_directory} && shards")
 
-  puts "Clear + Kemal template is now generated. `cd #{g.target_directory} && clear-cli server` to play ! :-)"
-end
+#   puts "Clear + Kemal template is now generated. `cd #{g.target_directory} && clear-cli server` to play ! :-)"
+# end
