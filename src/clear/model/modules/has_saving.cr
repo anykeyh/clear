d74b785d53ad5dd47243db565afe3dc9bb7cecba
Yacine Petitprez <yacine.petitprez@ingedata.net>
2018-12-30 15:04:54 +0700

    Setup Enumerable(T) instead of Array(T) in has_saving

--- src/clear/model/modules/has_saving.cr
+++ src/clear/model/modules/has_saving.cr
@@ -20,7 +20,7 @@ module Clear::Model::HasSaving
       #  users = [ User.new(id: 1), User.new(id: 2), User.new(id: 3)]
       #  users = User.import(users)
       # ```
-      def self.import(array : Array(self), on_conflict : (Clear::SQL::InsertQuery -> )? = nil)
+      def self.import(array : Enumerable(self), on_conflict : (Clear::SQL::InsertQuery -> )? = nil)
         array.each do |item|
           raise "One of your model is persisted while calling import" if item.persisted?
         end
