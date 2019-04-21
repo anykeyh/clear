303758f60b08839ef210b22f5297b2d2065e9987
Yacine Petitprez <anykeyh@gmail.com>
2019-04-21 13:30:50 +0700

    Fix styling issue

--- src/clear/sql/lock.cr
+++ src/clear/sql/lock.cr
@@ -2,7 +2,7 @@ module Clear
   module SQL
     # Tablewise locking
     def self.lock(table : String | Symbol, mode = "ACCESS EXCLUSIVE", connection = "default", &block)
-      Clear::SQL::ConnectionPool.with_connection(connection) do |cnx|
+      Clear::SQL::ConnectionPool.with_connection(connection) do |_|
         transaction do
           execute("LOCK TABLE #{table.to_s} IN #{mode} MODE")
           return yield
