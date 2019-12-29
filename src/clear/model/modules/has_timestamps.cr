b79aa98bd78afd17c22b348c98dfe9bf2770a4cd
Anton Maminov <anton.linux@gmail.com>
2019-12-29 15:40:47 +0200

    add touch method

--- src/clear/model/modules/has_timestamps.cr
+++ src/clear/model/modules/has_timestamps.cr
@@ -24,5 +24,13 @@ module Clear::Model::HasTimestamps
       model.updated_at = Time.local if model.changed? && !model.updated_at_column.changed?
     end
 
+    # Saves the record with the updated_at set to the current time.
+    def touch
+      now = Time.local
+
+      self.updated_at = Time.local
+      self.save
+    end
+
   end
 end
