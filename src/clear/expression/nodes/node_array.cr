4c4b46a3c95eaae3cb85d03da18eab48df898d3a
Yacine Petitprez <anykeyh@gmail.com>
2020-03-08 20:53:02 +0700

    Fixing where clause

---
+++ src/clear/expression/nodes/node_array.cr
@@ -0,0 +1,23 @@
+require "./node"
+
+# This node is used to generate expression like `( a AND b AND ... AND k )`
+class Clear::Expression::Node::NodeArray < Clear::Expression::Node
+  property expression : Array(Node)
+  property link : String
+
+  def initialize(expression : Array(Node), @link)
+    @expression = expression.dup
+  end
+
+  def resolve : String
+    if @expression.any?
+      {
+        "(",
+        @expression.map(&.resolve).join(" #{@link} "),
+        ")"
+      }.join
+    else
+      ""
+    end
+  end
+end
