419e054ed776efc7a44a89fb49df2fb7173fbe18
Duke Nguyen <duke@place.technology>
2020-09-23 10:29:08 +1000

    chore: spec update

--- spec/model/assign_from_json_spec.cr
+++ spec/model/assign_from_json_spec.cr
@@ -4,6 +4,7 @@ require "json"
 macro columns_to_instance_vars
   struct Assigner
     include JSON::Serializable
+
     {% for name, settings in COLUMNS %}
       getter {{name.id}} : {{settings[:type]}}?
     {% end %}
@@ -69,12 +70,18 @@ it "should create a new ItemTest", focus: true do
   i1_body = {title: "Pure Title", body: "Pure Body", published: false}
   i1 = ItemTest.pure_from_json(i1_body.to_json)
   i1.title.should eq(i1_body["title"])
+  i1.body.should eq(i1_body["body"])
+  i1.published.should eq(i1_body["published"])
 
   i2_body = {title: "New Title", body: "New Body", published: true}
   i2 = ItemTest.new_from_json(i2_body.to_json)
   i2.title.should eq(i2_body["title"])
+  i2.body.should eq(i2_body["body"])
+  i2.published.should eq(i2_body["published"])
 
   i3_body = {title: "Updated Title"}
   i3 = ItemTest.update_from_json(i2, i3_body.to_json)
   i3.title.should eq(i3_body["title"])
+  i3.body.should eq(i2_body["body"])
+  i3.published.should eq(i2_body["published"])
 end
