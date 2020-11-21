13568f7314ff76e7f1ecd24bb49588310010ff6b
Yacine Petitprez <anykeyh@gmail.com>
2020-11-21 17:59:22 +0700

    fix issues with interval now that PG gems handle them correctly

--- src/clear/extensions/time/interval.cr
+++ src/clear/extensions/time/interval.cr
@@ -65,6 +65,8 @@ struct Clear::Interval
   module Converter
     def self.to_column(x) : Clear::Interval?
       case x
+      when PG::Interval
+        Clear::Interval.new(months: x.months, days: x.days, microseconds: x.microseconds)
       when Slice # < Here bug of the crystal compiler with Slice(UInt8), do not want to compile
         Clear::Interval.decode(x.as(Slice(UInt8)))
       when Clear::Interval
