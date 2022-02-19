e4b6aced68ce80f61cbb6fd95cd3917b7e551ac0
Guilherme Bernal <guilherme@cubos.io>
2022-02-19 17:25:37 -0300

    fix: add PG::UUIDArray

--- src/clear/sql/sql.cr
+++ src/clear/sql/sql.cr
@@ -49,7 +49,7 @@ module Clear
     alias Any = Array(PG::BoolArray) | Array(PG::CharArray) | Array(PG::Float32Array) |
                 Array(PG::Float64Array) | Array(PG::Int16Array) | Array(PG::Int32Array) |
                 Array(PG::Int64Array) | Array(PG::StringArray) | Array(PG::TimeArray) |
-                Array(PG::NumericArray) |
+                Array(PG::NumericArray) | Array(PG::UUIDArray) |
                 Bool | Char | Float32 | Float64 | Int8 | Int16 | Int32 | Int64 | BigDecimal | JSON::PullParser | JSON::Any | JSON::Any::Type | PG::Geo::Box | PG::Geo::Circle |
                 PG::Geo::Line | PG::Geo::LineSegment | PG::Geo::Path | PG::Geo::Point |
                 PG::Geo::Polygon | PG::Numeric | PG::Interval | Slice(UInt8) | String | Time |
